57e3253c42dcd2e8950e6f8cfd85a3e857eea2b4
Malik Benkirane <overlay-flux.2t@icloud.com>
2026-05-15 07:57:05 +0100

    Add ModeSwitch widget with CupertinoSegmentedControl

---
+++ lib/widgets/mode_switch.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import '../models/timer_mode.dart';
+
+class ModeSwitch extends StatelessWidget {
+  const ModeSwitch({
+    super.key,
+    required this.currentMode,
+    required this.onModeChanged,
+  });
+
+  final TimerMode currentMode;
+  final ValueChanged<TimerMode> onModeChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSegmentedControl<TimerMode>(
+      groupValue: currentMode,
+      onValueChanged: onModeChanged,
+      children: {
+        for (final mode in TimerMode.values)
+          mode: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(mode.label),
+          ),
+      },
+    );
+  }
+}
