8d92353521719438336f12445522e9cf015506e0
Malik Benkirane <mbenkirane@internetworks.blog>
2026-07-08 20:45:19 +0100

    refactor(main): wire BlocProvider tree via AppDeps

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,47 @@
 import 'package:flutter/cupertino.dart';
-import 'timer_screen.dart';
-import 'notifications.dart' as notifications;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vipo/blocs/logs/logs_bloc.dart';
+import 'package:vipo/blocs/notes/notes_bloc.dart';
+import 'package:vipo/blocs/timer/timer_bloc.dart';
+import 'package:vipo/di.dart';
+import 'package:vipo/notifications.dart' as notifications;
+import 'package:vipo/repositories/logs_repository.dart';
+import 'package:vipo/repositories/notes_repository.dart';
+import 'package:vipo/timer_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize notifications
   await notifications.initialize();
 
-  runApp(const VipoApp());
+  runApp(VipoApp(deps: AppDeps()));
 }
 
 class VipoApp extends StatelessWidget {
-  const VipoApp({super.key});
+  const VipoApp({super.key, required this.deps});
+
+  final AppDeps deps;
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
       title: 'Vipo',
       debugShowCheckedModeBanner: false,
-      theme: CupertinoThemeData(brightness: Brightness.dark),
-      home: TimerScreen(),
+      theme: const CupertinoThemeData(brightness: Brightness.dark),
+      home: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<LogsRepository>.value(value: deps.logsRepository),
+          RepositoryProvider<NotesRepository>.value(value: deps.notesRepository),
+        ],
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<LogsBloc>(create: (_) => deps.logsBloc),
+            BlocProvider<TimerBloc>(create: (_) => deps.timerBloc),
+            BlocProvider<NotesBloc>(create: (_) => deps.notesBloc),
+          ],
+          child: const TimerScreen(),
+        ),
+      ),
     );
   }
 }
